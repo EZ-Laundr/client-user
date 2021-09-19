1a983ce35aa3b5ce1578f31cb9e46651ddfca305
gregorius-tech <gregoriusverly@gmail.com>
2021-09-19 17:47:36 +0700

    cart layouting ok

--- user/src/screens/Cart.jsx
+++ user/src/screens/Cart.jsx
@@ -5,15 +5,21 @@ import {
   View,
   ScrollView,
   Image,
+  Dimensions,
   TextInput,
   TouchableHighlight,
 } from "react-native";
-import { DataTable } from "react-native-paper";
+const windowWidth = Dimensions.get("window").width;
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { useDispatch } from "react-redux";
+import { createOrder } from "../store/action";
 const optionsPerPage = [2, 3, 4];
 export default function Cart({ route, navigation }) {
   const [page, setPage] = React.useState(0);
   const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
+  const dispatch = useDispatch();
   // const { cartData } = route.params;
+
   const cartData = {
     perfume: {
       id: 2,
@@ -45,51 +51,95 @@ export default function Cart({ route, navigation }) {
     ],
   };
 
+  function chechoutHander() {
+    const payload = {
+      ServiceId: cartData.service.id,
+      perfume: {
+        id: cartData.perfume.id,
+        price: cartData.perfume.price,
+      },
+      treatments: cartData.treatments,
+      pickup: cartData.pickup,
+    };
+
+    dispatch(createOrder(payload));
+
+    navigation.navigate("OrderCompleted");
+  }
+
+  function convertToRupiah(angka) {
+    var rupiah = "";
+    var angkarev = angka.toString().split("").reverse().join("");
+    for (var i = 0; i < angkarev.length; i++)
+      if (i % 3 == 0) rupiah += angkarev.substr(i, 3) + ".";
+    return (
+      "Rp. " +
+      rupiah
+        .split("", rupiah.length - 1)
+        .reverse()
+        .join("")
+    );
+  }
   React.useEffect(() => {
     setPage(0);
   }, [itemsPerPage]);
   return (
     <>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>Item</DataTable.Title>
-          <DataTable.Title numeric>Qty</DataTable.Title>
-          <DataTable.Title numeric>Price</DataTable.Title>
-        </DataTable.Header>
-
-        <DataTable.Row>
-          <DataTable.Cell>{cartData.service.title}</DataTable.Cell>
-          <DataTable.Cell numeric>-</DataTable.Cell>
-          <DataTable.Cell numeric>-</DataTable.Cell>
-        </DataTable.Row>
-
-        <DataTable.Row>
-          <DataTable.Cell>{cartData.perfume.title}</DataTable.Cell>
-          <DataTable.Cell numeric>-</DataTable.Cell>
-          <DataTable.Cell numeric>{cartData.perfume.price}</DataTable.Cell>
-        </DataTable.Row>
-
-        {cartData.treatments.map((treat) => {
-          return (
-            <DataTable.Row>
-              <DataTable.Cell>{treat.title}</DataTable.Cell>
-              <DataTable.Cell numeric>-</DataTable.Cell>
-              <DataTable.Cell numeric>{treat.price}</DataTable.Cell>
-            </DataTable.Row>
-          );
-        })}
-        <DataTable.Pagination
-          page={page}
-          numberOfPages={3}
-          onPageChange={(page) => setPage(page)}
-          label="1-2 of 6"
-          optionsPerPage={optionsPerPage}
-          itemsPerPage={itemsPerPage}
-          setItemsPerPage={setItemsPerPage}
-          showFastPagination
-          optionsLabel={"Rows per page"}
-        />
-      </DataTable>
+      <Card>
+        <Card.Title title={cartData.service.title} />
+        <Card.Cover source={{ uri: `${cartData.service.image}` }} />
+        <Card.Content>
+          <View>
+            <Title>With perfume</Title>
+            <Paragraph>{cartData.perfume.title}</Paragraph>
+            <View>
+              <Image
+                style={{ width: 80, height: 80, borderRadius: 20 }}
+                source={{ uri: `${cartData.perfume.image}` }}
+              />
+              <Text>{convertToRupiah(+cartData.perfume.price)}</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: "row", marginTop: 10 }}>
+            {cartData.treatments.map((treat) => {
+              return (
+                <View>
+                  <Paragraph>{treat.title}</Paragraph>
+                  <View>
+                    <Image
+                      style={{ width: 80, height: 80, borderRadius: 20 }}
+                      source={{ uri: `${treat.image}` }}
+                    />
+                    <Text>{convertToRupiah(+cartData.perfume.price)}</Text>
+                  </View>
+                </View>
+              );
+            })}
+          </View>
+        </Card.Content>
+        <Card.Actions></Card.Actions>
+      </Card>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          bottom: 3,
+          position: "absolute",
+        }}
+      >
+        <Button
+          labelStyle={{ fontSize: 20, textAlign: "center" }}
+          style={{
+            width: windowWidth,
+            height: 40,
+            borderRadius: 0,
+          }}
+          mode="contained"
+          onPress={() => chechoutHander()}
+        >
+          Checkout
+        </Button>
+      </View>
     </>
   );
 }
