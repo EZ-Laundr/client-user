f67fb033a7cd7f028380b16a32103a03e84594b2
Garry <Garry@Ivys-MacBook-2.local>
2021-09-22 03:30:23 +0700

    testulang

--- user/src/screens/Splash.jsx
+++ user/src/screens/Splash.jsx
@@ -6,30 +6,33 @@ import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 import { useDispatch } from "react-redux";
 import { setLoading } from "../store/action";
 export default function Splash({ navigation }) {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    setTimeout(() => {
-      dispatch(setLoading(true));
-      navigation.replace("Register");
-    }, 3000);
-  }, [navigation]);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        setTimeout(() => {
+            dispatch(setLoading(true));
+            navigation.replace("Ez Loundr");
+        }, 3000);
+    }, [navigation]);
 
-  return (
-    <View style={styles.background}>
-      <Image style={styles.logo} source={require("../img/EZ_Laundr.png")} />
-    </View>
-  );
+    return (
+        <View style={styles.background}>
+            <Image
+                style={styles.logo}
+                source={require("../img/EZ_Laundr.png")}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
-  },
-  logo: {
-    width: 222,
-    height: 105,
-  },
+    background: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+    },
+    logo: {
+        width: 222,
+        height: 105,
+    },
 });
