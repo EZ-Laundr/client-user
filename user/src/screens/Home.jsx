59bbf150cb6cb0bfc4c8f0dcf170337785782620
ariefkoko <arief.renaldy16@gmail.com>
2021-09-22 15:14:37 +0700

    pushhh

--- user/src/screens/Home.jsx
+++ user/src/screens/Home.jsx
@@ -26,18 +26,6 @@ import { Loading } from "../components/LoadingPage";
 
 import MyCarousel from "../components/Homee";
 
-import Constants from "expo-constants";
-// import * as Notifications from "expo-notifications";
-// import { useState, useRef } from "react";
-
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => ({
-//     shouldShowAlert: true,
-//     shouldPlaySound: false,
-//     shouldSetBadge: false,
-//   }),
-// });
-
 export default function Home({ navigation }) {
   const dispatch = useDispatch();
   const { userId, services, perfumes, treatments, access_token, loading } =
@@ -46,29 +34,6 @@ export default function Home({ navigation }) {
   const [refreshing, setRefreshing] = React.useState(false);
   const onStateChange = ({ open }) => setState({ open });
   const { open } = state;
-//   const [notification, setNotification] = useState(false);
-//   const notificationListener = useRef();
-//   const responseListener = useRef();
-
-//   useEffect(() => {
-//     notificationListener.current =
-//       Notifications.addNotificationReceivedListener((notification) => {
-//         setNotification(notification);
-//       });
-
-//     responseListener.current =
-//       Notifications.addNotificationResponseReceivedListener((response) => {
-//         console.log(response, 2);
-//         navigation.navigate("Ez Loundr");
-//       });
-
-//     return () => {
-//       Notifications.removeNotificationSubscription(
-//         notificationListener.current
-//       );
-//       Notifications.removeNotificationSubscription(responseListener.current);
-//     };
-//   }, []);
 
   useEffect(() => {
     dispatch(fetchServices());
