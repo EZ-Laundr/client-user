61d1df0362f0c69f3d9aa79a0471a4466f2aa447
Garry <Garry@Ivys-MacBook-2.local>
2021-09-23 07:51:19 +0700

    home-garry

--- user/src/components/CardService.jsx
+++ user/src/components/CardService.jsx
@@ -1,47 +1,327 @@
-import React from "react";
-import { Dimensions, Text, View, Image } from "react-native";
-import { Avatar, Button, Card, Title, Chip } from "react-native-paper";
+import React, { useState } from "react";
+import {
+    Dimensions,
+    Text,
+    View,
+    Image,
+    FlatList,
+    TouchableOpacity,
+    TouchableHighlight,
+    SafeAreaView,
+    ScrollView,
+    Modal,
+} from "react-native";
+import Card from "react-native-card-component";
+import { Avatar, Button, Title, Chip } from "react-native-paper";
 const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 import { useDispatch, useSelector } from "react-redux";
-export default function CardService({ service, navigation }) {
-  const { services, access_token, loading } = useSelector(
-    (state) => state.reducer
-  );
 
-  return (
-    <View>
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Card
-          style={{ width: 180, height: 180, margin: 5, alignItems: "center" }}
+export default function CardService({ service, navigation }) {
+    const { services, access_token, loading, perfumes, treatments } =
+        useSelector((state) => state.reducer);
+
+    // setModalVisible(visible) {
+    //   this.setState({modalVisible: visible});
+    // }
+
+    const cardGap = 20;
+
+    const cardWidth = (Dimensions.get("window").width - cardGap * 3) / 4;
+
+    return (
+        <ScrollView
+            contentContainerStyle={{
+                paddingTop: 15,
+            }}
         >
-          <Card.Cover
-            style={{ width: 120, height: 120, marginTop: 10 }}
-            source={{ uri: `${service.imageUrl}` }}
-          />
-          <Card.Actions>
-            <Chip
-              labelStyle={{ fontSize: 10 }}
-              style={{
-                width: 100,
-                height: 30,
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "#3DB2FF",
-                borderRadius: 0,
-                justifyContent: "space-evenly",
-              }}
-              mode="contained"
+            {/* <Image
+                style={{
+                    width: 190,
+                    height: 90,
+                    alignSelf: "center",
+                }}
+                source={require("../assets/head4_white.png")}
+            /> */}
+            <Text
+                style={{
+                    alignSelf: "flex-start",
+                    marginLeft: 16,
+                    fontWeight: "bold",
+                    fontSize: 15,
+                }}
             >
-              {service.name}
-            </Chip>
-          </Card.Actions>
-        </Card>
-      </View>
-    </View>
-  );
+                Layanan Kami
+            </Text>
+            <View
+                style={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "space-evenly",
+                }}
+            >
+                <View
+                    style={{
+                        marginTop: cardGap,
+                        widthType: "auto",
+                        heightType: "auto",
+                        backgroundColor: "white",
+                        borderRadius: 16,
+                        shadowOpacity: 0.2,
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        padding: 8,
+                    }}
+                >
+                    <TouchableOpacity>
+                        <Image
+                            source={{
+                                uri: "https://i.pinimg.com/564x/8b/63/32/8b6332b31be6fb6121daf3fde2f8d74c.jpg",
+                            }}
+                            style={{
+                                width: 60,
+                                height: 60,
+                                borderRadius: 16,
+                                overflow: "hidden",
+                            }}
+                        />
+                    </TouchableOpacity>
+                    {/* <Text style={{ alignSelf: "center" }}>
+                                {service.name}
+                            </Text> */}
+                </View>
+                <View
+                    style={{
+                        marginTop: cardGap,
+                        widthType: "auto",
+                        heightType: "auto",
+                        backgroundColor: "white",
+                        borderRadius: 16,
+                        shadowOpacity: 0.2,
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        padding: 8,
+                    }}
+                >
+                    <TouchableOpacity>
+                        <Image
+                            source={{
+                                uri: "https://thumbs.dreamstime.com/z/iron-cartoon-icon-white-background-79695409.jpg",
+                            }}
+                            style={{
+                                width: 60,
+                                height: 60,
+                                borderRadius: 16,
+                                overflow: "hidden",
+                            }}
+                        />
+                    </TouchableOpacity>
+                    {/* <Text style={{ alignSelf: "center" }}>
+                                {service.name}
+                            </Text> */}
+                </View>
+                <View
+                    style={{
+                        marginTop: cardGap,
+                        widthType: "auto",
+                        heightType: "auto",
+                        backgroundColor: "white",
+                        borderRadius: 16,
+                        shadowOpacity: 0.2,
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        padding: 8,
+                    }}
+                >
+                    <TouchableOpacity>
+                        <Image
+                            source={{
+                                uri: "https://media.istockphoto.com/vectors/household-chores-iron-board-and-washing-machine-flat-editable-vector-vector-id851824906?s=612x612",
+                            }}
+                            style={{
+                                width: 60,
+                                height: 60,
+                                borderRadius: 16,
+                                overflow: "hidden",
+                            }}
+                        />
+                    </TouchableOpacity>
+                    {/* <Text style={{ alignSelf: "center" }}>
+                                {service.name}
+                            </Text> */}
+                </View>
+                <View
+                    style={{
+                        marginTop: cardGap,
+                        widthType: "auto",
+                        heightType: "auto",
+                        backgroundColor: "white",
+                        borderRadius: 16,
+                        shadowOpacity: 0.2,
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        padding: 8,
+                    }}
+                >
+                    <TouchableOpacity>
+                        <Image
+                            source={{
+                                uri: "https://thumbs.dreamstime.com/z/super-hero-washing-machine-10210035.jpg",
+                            }}
+                            style={{
+                                width: 60,
+                                height: 60,
+                                borderRadius: 16,
+                                overflow: "hidden",
+                            }}
+                        />
+                    </TouchableOpacity>
+                    {/* <Text style={{ alignSelf: "center" }}>
+                                {service.name}
+                            </Text> */}
+                </View>
+            </View>
+            <Text
+                style={{
+                    alignSelf: "flex-start",
+                    marginLeft: 16,
+                    fontWeight: "bold",
+                    fontSize: 15,
+                    marginTop: 15,
+                }}
+            >
+                Pilihan Parfum
+            </Text>
+            <View
+                style={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "space-evenly",
+                }}
+            >
+                {perfumes.map((perfume) => {
+                    return (
+                        <View
+                            style={{
+                                marginTop: cardGap,
+                                width: cardWidth,
+                                backgroundColor: "white",
+                                borderRadius: 16,
+                                shadowOpacity: 0.2,
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                                padding: 8,
+                            }}
+                        >
+                            <TouchableOpacity>
+                                <Image
+                                    source={{
+                                        uri: perfume.imageUrl,
+                                    }}
+                                    style={{
+                                        width: 60,
+                                        height: 60,
+                                        borderRadius: 16,
+                                        overflow: "hidden",
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    );
+                })}
+            </View>
+            <Text
+                style={{
+                    alignSelf: "flex-start",
+                    marginLeft: 16,
+                    fontWeight: "bold",
+                    fontSize: 15,
+                    marginTop: 15,
+                }}
+            >
+                Special Treatments
+            </Text>
+            <View
+                style={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "space-evenly",
+                }}
+            >
+                {treatments.map((perfume) => {
+                    return (
+                        <View
+                            style={{
+                                marginTop: cardGap,
+                                marginBottom: cardGap,
+                                width:
+                                    (Dimensions.get("window").width -
+                                        cardGap * 3) /
+                                    3,
+                                backgroundColor: "white",
+                                borderRadius: 16,
+                                shadowOpacity: 0.2,
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                                padding: 8,
+                            }}
+                        >
+                            <TouchableOpacity>
+                                <Image
+                                    source={{
+                                        uri: perfume.imageUrl,
+                                    }}
+                                    style={{
+                                        width: 60,
+                                        height: 60,
+                                        borderRadius: 16,
+                                        overflow: "hidden",
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    );
+                })}
+            </View>
+        </ScrollView>
+
+        // <View>
+        //     <View
+        //         style={{
+        //             alignItems: "center",
+        //             justifyContent: "space-between",
+        //         }}
+        //     >
+        //         <Card
+        // style={{
+        //     width: 180,
+        //     height: 180,
+        //     margin: 5,
+        //     alignItems: "center",
+        // }}
+        //         >
+        // <Card.Cover
+        //     style={{ width: 120, height: 120, marginTop: 10 }}
+        //     source={{ uri: `${service.imageUrl}` }}
+        // />
+        //             <Card.Actions>
+        //                 <Chip
+        //                     labelStyle={{ fontSize: 10 }}
+        //                     style={{
+        //                         width: 100,
+        //                         height: 30,
+        //                         alignItems: "center",
+        //                         justifyContent: "center",
+        //                         backgroundColor: "#3DB2FF",
+        //                         borderRadius: 0,
+        //                         justifyContent: "space-evenly",
+        //                     }}
+        //                     mode="contained"
+        //                 >
+        //                     {service.name}
+        //                 </Chip>
+        //             </Card.Actions>
+        //         </Card>
+        //     </View>
+        // </View>
+    );
 }
--- user/src/screens/Home.jsx
+++ user/src/screens/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import {
-  Dimensions,
-  Text,
-  View,
-  ScrollView,
-  Alert,
-  RefreshControl,
+    Dimensions,
+    Text,
+    View,
+    ScrollView,
+    Alert,
+    RefreshControl,
 } from "react-native";
 import CardService from "../components/CardService";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -15,11 +15,11 @@ import { Button, FAB, Portal, Provider } from "react-native-paper";
 import CarouselItem from "../components/CarouselItem";
 import {
     deleteToken,
-  fetchParfume,
-  fetchServices,
-  fetchTreatment,
-  setLoading,
-  setToken,
+    fetchParfume,
+    fetchServices,
+    fetchTreatment,
+    setLoading,
+    setToken,
 } from "../store/action";
 
 import { Loading } from "../components/LoadingPage";
@@ -27,145 +27,147 @@ import { Loading } from "../components/LoadingPage";
 import MyCarousel from "../components/Homee";
 
 export default function Home({ navigation }) {
-  const dispatch = useDispatch();
-  const { userId, services, perfumes, treatments, access_token, loading } =
-    useSelector((state) => state.reducer);
-  const [state, setState] = React.useState({ open: false });
-  const [refreshing, setRefreshing] = React.useState(false);
-  const onStateChange = ({ open }) => setState({ open });
-  const { open } = state;
+    const dispatch = useDispatch();
+    const { userId, services, perfumes, treatments, access_token, loading } =
+        useSelector((state) => state.reducer);
+    const [state, setState] = React.useState({ open: false });
+    const [refreshing, setRefreshing] = React.useState(false);
+    const onStateChange = ({ open }) => setState({ open });
+    const { open } = state;
 
-  useEffect(() => {
-    dispatch(fetchServices());
-    dispatch(fetchParfume());
-    dispatch(fetchTreatment());
-    dispatch(setLoading(false));
-  }, []);
+    useEffect(() => {
+        dispatch(fetchServices());
+        dispatch(fetchParfume());
+        dispatch(fetchTreatment());
+        dispatch(setLoading(false));
+    }, []);
 
-  const wait = (timeout) => {
-    return new Promise((resolve) => {
-      setTimeout(resolve, timeout);
-    });
-  };
+    const wait = (timeout) => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, timeout);
+        });
+    };
 
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    dispatch(fetchServices());
-    dispatch(fetchParfume());
-    dispatch(fetchTreatment());
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        dispatch(fetchServices());
+        dispatch(fetchParfume());
+        dispatch(fetchTreatment());
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
 
-  function createOrderHandler() {
-    if (access_token == "") {
-      Alert.alert("Login Status", "Kamu harus login dulu", [
-        {
-          text: "Login",
-          onPress: () => {
-            dispatch(setLoading(true));
-            navigation.navigate("Login");
-            console.log("jalan");
-          },
-          style: "ok",
-        },
-        {
-          text: "Cancel",
-          style: "cancel",
-        },
-      ]);
-    } else {
-      navigation.navigate("Create Order");
+    function createOrderHandler() {
+        if (access_token == "") {
+            Alert.alert("Login Status", "Kamu harus login dulu", [
+                {
+                    text: "Login",
+                    onPress: () => {
+                        dispatch(setLoading(true));
+                        navigation.navigate("Login");
+                        console.log("jalan");
+                    },
+                    style: "ok",
+                },
+                {
+                    text: "Cancel",
+                    style: "cancel",
+                },
+            ]);
+        } else {
+            navigation.navigate("Create Order");
+        }
     }
-  }
 
-  function logoutHandler() {
-    dispatch(deleteToken())
-    dispatch(setToken(""));
-    Alert.alert("Logout Sukses", "Kamu berhasil logout!");
-  }
+    function logoutHandler() {
+        dispatch(deleteToken());
+        dispatch(setToken(""));
+        Alert.alert("Logout Sukses", "Kamu berhasil logout!");
+    }
 
-  return (
-    <>
-      {console.log(userId, "userID")}
-      <Provider>
-        <View style={{ flex: 1, backgroundColor: "#1EAFED" }}>
-          <View style={{ height: height * 0.45 }}>
-            <CarouselItem />
-          </View>
-          <MyCarousel item={services} />
-        </View>
-        <Portal>
-          {access_token == "" ? (
-            <FAB.Group
-              fabStyle={{ backgroundColor: "#3DB2FF" }}
-              color="#FFFF"
-              style={{ zIndex: 50 }}
-              open={open}
-              icon={open ? "basket-outline" : "plus"}
-              actions={[
-                {
-                  icon: "login",
-                  label: "Login",
-                  onPress: (e) => {
-                    dispatch(setLoading(true)), console.log("jalan");
-                    navigation.navigate("Login");
-                  },
-                  small: false,
-                },
+    return (
+        <>
+            {console.log(userId, "userID")}
+            <Provider>
+                <View style={{ flex: 1, backgroundColor: "#1EAFED" }}>
+                    <View style={{ height: height * 0.3 }}>
+                        <CarouselItem />
+                    </View>
+                    <CardService />
+                </View>
+                <Portal>
+                    {access_token == "" ? (
+                        <FAB.Group
+                            fabStyle={{ backgroundColor: "#3DB2FF" }}
+                            color="#FFFF"
+                            style={{ zIndex: 50 }}
+                            open={open}
+                            icon={open ? "basket-outline" : "plus"}
+                            actions={[
+                                {
+                                    icon: "login",
+                                    label: "Login",
+                                    onPress: (e) => {
+                                        dispatch(setLoading(true)),
+                                            console.log("jalan");
+                                        navigation.navigate("Login");
+                                    },
+                                    small: false,
+                                },
 
-                {
-                  icon: "washing-machine",
-                  label: "Pesan Laundry",
-                  onPress: () => createOrderHandler(),
-                  small: false,
-                },
-              ]}
-              onStateChange={onStateChange}
-              onPress={() => {
-                if (open) {
-                  // do something if the speed dial is open
-                }
-              }}
-            />
-          ) : (
-            <FAB.Group
-              fabStyle={{ backgroundColor: "#3DB2FF" }}
-              color="#FFFF"
-              style={{ zIndex: 50 }}
-              open={open}
-              icon={open ? "basket-outline" : "plus"}
-              actions={[
-                {
-                  icon: "logout",
-                  label: "Logout",
-                  onPress: () => logoutHandler(),
-                  small: false,
-                },
-                {
-                  icon: "chat",
-                  label: "Chat Admin",
-                  onPress: () => navigation.navigate("Chat Admin"),
-                  small: false,
-                },
-                {
-                  icon: "washing-machine",
-                  label: "Pesan Laundry",
-                  onPress: () => createOrderHandler(),
-                  small: false,
-                },
-              ]}
-              onStateChange={onStateChange}
-              onPress={() => {
-                if (open) {
-                  // do something if the speed dial is open
-                }
-              }}
-            />
-          )}
-        </Portal>
-      </Provider>
-    </>
-  );
+                                {
+                                    icon: "washing-machine",
+                                    label: "Pesan Laundry",
+                                    onPress: () => createOrderHandler(),
+                                    small: false,
+                                },
+                            ]}
+                            onStateChange={onStateChange}
+                            onPress={() => {
+                                if (open) {
+                                    // do something if the speed dial is open
+                                }
+                            }}
+                        />
+                    ) : (
+                        <FAB.Group
+                            fabStyle={{ backgroundColor: "#3DB2FF" }}
+                            color="#FFFF"
+                            style={{ zIndex: 50 }}
+                            open={open}
+                            icon={open ? "basket-outline" : "plus"}
+                            actions={[
+                                {
+                                    icon: "logout",
+                                    label: "Logout",
+                                    onPress: () => logoutHandler(),
+                                    small: false,
+                                },
+                                {
+                                    icon: "chat",
+                                    label: "Chat Admin",
+                                    onPress: () =>
+                                        navigation.navigate("Chat Admin"),
+                                    small: false,
+                                },
+                                {
+                                    icon: "washing-machine",
+                                    label: "Pesan Laundry",
+                                    onPress: () => createOrderHandler(),
+                                    small: false,
+                                },
+                            ]}
+                            onStateChange={onStateChange}
+                            onPress={() => {
+                                if (open) {
+                                    // do something if the speed dial is open
+                                }
+                            }}
+                        />
+                    )}
+                </Portal>
+            </Provider>
+        </>
+    );
 }
 
 // <ScrollView
